4759a734fdcb05d98878a3f912b683e1c71ddfcd
GodDamnMan <shariev.marat@mail.ru>
2025-11-22 14:28:38 +0300

    fix: workflow dockerfiles are now on context

--- docker-build.local.sh
+++ docker-build.local.sh
@@ -1,4 +1,4 @@
 docker rmi -f colorize/api
 docker rmi -f colorize/web
-docker build -f ./src/deployment/api/Dockerfile -t colorize/api .
-docker build -f ./src/deployment/app/Dockerfile -t colorize/web .
+docker build -f ./src/deployment/api/Dockerfile.local -t colorize/api .
+docker build -f ./src/deployment/app/Dockerfile.local -t colorize/web .
